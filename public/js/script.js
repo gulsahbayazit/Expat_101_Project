1a7d60b577eb8a568ecd8069ae080ff6dd8f39a5
gulsahbayazit <bayazitgulsahbayazit@gmail.com>
2023-02-13 12:02:54 +0100

    second commit

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Expat_101 JS imported successfully!");
+});
