2a611814f843cc288a6ddd9b4fd6414461fc8462
Ulkar Isayeva <ulkar.isayeva@gmail.com>
2023-02-15 17:40:35 +0100

    createpage and database

--- models/Recommendation.js
+++ models/Recommendation.js
@@ -17,17 +17,28 @@ const recommendationSchema = new Schema(
     },
     tags: {
       type: String,
+      required: true,
     },
-    comment: {
+    category: {
       type: String,
-      required: true,
     },
-    rate: {
-      type: Number,
-      min: 1,
-      max: 5,
-      required: true,
+    imgPath: {
+      type: String,
     },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+    // comment: {
+    //   type: String,
+    //   required: true,
+    // },
+    // rate: {
+    //   type: Number,
+    //   min: 1,
+    //   max: 5,
+    //   required: true,
+    // },
   },
   // this second object adds extra properties: `createdAt` and `updatedAt`
   { timestamps: true }
